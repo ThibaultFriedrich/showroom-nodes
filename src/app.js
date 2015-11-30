6c8541c403a63656619d6026108a2734678ad3f5
Thibault Friedrich <tf@elqui.fr>
2015-11-30 23:58:55 +0100

    add animate.css

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 
-var app = angular.module('showroomNodes', ['ngResource','ngMaterial']);
+var app = angular.module('showroomNodes', ['ngResource','ngMaterial','ngMdIcons']);
 app.controller('mainCtrl', function($scope) {
 
-$scope.hello = 'world';
+    var typeOfMove = 'bounceOut';
+
+    $scope.direction = '';
+
+    $scope.move = '';
+
+    setTimeout(function(){
+        //console.log('timeout');
+        //if($scope.direction == 'left'){
+            //$scope.move = typeOfMove+'Left';
+        //} else if($scope.direction == 'right'){
+            //$scope.move = typeOfMove+'Right';
+        //}
+        //console.log($scope.move);
+        $scope.direction = 'left';
+    }, 2000);
 
 });
